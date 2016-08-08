6d3bb983af8b458c33257938aabc81c328bf93ed
Samuel Heinz <samueljheinz@gmail.com>
2016-08-08 14:34:45 -0700

    bubble_sort now returns a properly sorted array

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,4 +1,18 @@
 def bubble_sort(arr)
 	return arr if arr.length <= 1
+	n = arr.length
 
+	switch = true
+
+	while switch == true
+		switch = false
+		(0...n-1) do |i|
+			if arr[i] > arr[i + 1]
+				arr[i], arr[i + 1] = arr[i + 1], arr[i]
+				switch = true
+			end
+		end
+	end
+
+	return arr
 end
