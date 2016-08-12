bd67b65bf0bf33c179cd0079401b2106e67c1d3f
Samuel Heinz <samueljheinz@gmail.com>
2016-08-11 18:00:34 -0700

    Removed comments

--- quick_sort.rb
+++ quick_sort.rb
@@ -2,11 +2,10 @@ def quick_sort(arr)
 	if arr.length <= 1 
 		return arr
 	else
-		# pivot_num = arr[-1]
+
 		pivot_sample = (arr.sample(2))
 		pivot_num = arr.inject{ |sum, element| sum + element }.to_f / 2
-		# pivot_index = arr.index(pivot_num)
-		# arr.delete_at(pivot_index)
+
 		left = []
 		right = []
 		
@@ -20,7 +19,6 @@ def quick_sort(arr)
 		
 		sorted_arr = []
 		sorted_arr << quick_sort(left)
-		# sorted_arr << pivot_num
 		sorted_arr << quick_sort(right)
 	end
 	
